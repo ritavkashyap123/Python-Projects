98d130b85b30f2cd1c33c3fb7dd7c6727b68f3b0
Ritav Kashyap <79644483+ritavkashyap123@users.noreply.github.com>
2021-11-15 01:52:14 +0530

    Tictak updated

--- Face_detection.py
+++ Face_detection.py
@@ -1,17 +1,15 @@
 import cv2
 import sys
 
-imagePath = sys.argv[1]
-cascPath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascPath)
-image = cv2.imread(imagePath)
+imagepath = sys.argv[1:]
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+image = cv2.imread("https://www.compassion.com/Images/potw-2016-week-18.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 faces = faceCascade.detectMultiScale(
     gray,
     scaleFactor = 1.1,
     minNeighbors = 5,
     minSize = (30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
 )
 
 print("Found {0} faces!".format(len(faces)))
--- tictak.py
+++ tictak.py
@@ -2,11 +2,11 @@ import random
 import sys
 board=[i for i in range(0,9)]
 player, computer = '',''
-// Corners, Center and Others, respectively
+#Corners, Center and Others, respectively
 moves=((1,7,3,9),(5,),(2,4,6,8))
-// Winner combinations
+#Winner combinations
 winners=((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
-// Table
+#Table
 tab=range(1,10)
 def print_board():
     x=1
@@ -52,22 +52,22 @@ def make_move(brd, player, move, undo=False):
             brd[move-1] = move-1
         return (True, win)
     return (False, False)
-// AI goes here
+#AI goes here
 def computer_move():
     move=-1
-    // If I can win, others do not matter.
+    #If I can win, others do not matter.
     for i in range(1,10):
         if make_move(board, computer, i, True)[1]:
             move=i
             break
     if move == -1:
-       // If player can win, block him.
+       #If player can win, block him.
         for i in range(1,10):
             if make_move(board, player, i, True)[1]:
                 move=i
                 break
     if move == -1:
-        // Otherwise, try to take one of desired places.
+        #Otherwise, try to take one of desired places.
         for tup in moves:
             for mv in tup:
                 if move == -1 and can_move(board, computer, mv):
@@ -87,7 +87,6 @@ while space_exist():
     if not moved:
         print(' >> Invalid number ! Try again !')
         continue
-    //
     if won:
         result='*** Congratulations ! You won ! ***'
         break
